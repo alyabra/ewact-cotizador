43f580780580f58694e48c995c825795f868aaab
a-horse-with-no-name <alyabra.vargas@gmail.com>
2022-10-30 17:14:33 -0600

    Formulario creado

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import AppSeguro from './components/AppSeguro'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <AppSeguro />
+  )
+}
+
+export default App
